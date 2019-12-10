2650be3ff4c061eba3cefd0987b1ecd4880a1f7b
Franck Cornu <franck.cornu@outlook.com>
2019-12-10 12:31:38 -0500

    - Fixed #31

--- search-parts/src/services/DynamicDataService/DynamicDataService.ts
+++ search-parts/src/services/DynamicDataService/DynamicDataService.ts
@@ -10,47 +10,6 @@ export class DynamicDataService implements IDynamicDataService {
     constructor(dynamicDataProvider: DynamicDataProvider) {
         this._dynamicDataProvider = dynamicDataProvider;
     }
-
-    public getDataSourceValue<TValue>(dynamicProperty: DynamicProperty<TValue>, sourceId: string, propertyId: string, propertyPath: string): TValue {
-        let dataSourceValue: TValue;
-        let source = dynamicProperty.tryGetSource();
-
-        // Try to get the source if a source ID is present
-        // We need to do this check to avoid timing issues regarding data sources reconenction
-        if (!source && sourceId) {
-            source = this._dynamicDataProvider.tryGetSource(sourceId);
-
-            if (source && propertyId) {
-                dataSourceValue = source.getPropertyValue(propertyId)[propertyPath];
-            }
-
-        } else {
-            dataSourceValue = dynamicProperty.tryGetValue();
-        }
-
-        return dataSourceValue;
-    }    
-    
-    public getDataSourceValues<TValue>(dynamicProperty: DynamicProperty<TValue>, sourceId: string, propertyId: string, propertyPath: string): TValue[] {
-        let dataSourceValue: TValue[];
-        let source = dynamicProperty.tryGetSource();
-
-        // Try to get the source if a source ID is present
-        // We need to do this check to avoid timing issues regarding data sources reconenction
-        if (!source && sourceId) {
-            source = this._dynamicDataProvider.tryGetSource(sourceId);
-
-            if (source && propertyId) {
-                dataSourceValue = source.getPropertyValue(propertyId)[propertyPath];
-            }
-
-        } else {
-            dataSourceValue = dynamicProperty.tryGetValues();
-        }
-
-        return dataSourceValue;
-    }
-
     
     /**
      * Get available data sources on the page with specific property Id (i.e. corresponding to the underlying component type)
