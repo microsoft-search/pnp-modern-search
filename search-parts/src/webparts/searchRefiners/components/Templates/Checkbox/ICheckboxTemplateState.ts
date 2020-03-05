289e832d7e5ed089e2304ac8b536002a6fd06284
Joel Rodrigues <19577724+joelfmrodrigues@users.noreply.github.com>
2020-03-05 14:44:09 +0000

    remove interface property already inherited

--- search-parts/src/webparts/searchRefiners/components/Templates/Checkbox/ICheckboxTemplateState.ts
+++ search-parts/src/webparts/searchRefiners/components/Templates/Checkbox/ICheckboxTemplateState.ts
@@ -4,11 +4,6 @@ import { IRefinementValue } from "../../../../../models/ISearchResult";
 interface ICheckboxTemplateState extends IBaseRefinerTemplateState {
 
     /**
-     * The current selected values for the refiner
-     */
-    refinerSelectedFilterValues: IRefinementValue[];
-
-    /**
      * The current filtered values for the refiner
      */
     refinerFilteredFilterValues: IRefinementValue[];
