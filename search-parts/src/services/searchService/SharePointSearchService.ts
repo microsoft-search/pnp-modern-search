54f27cd9840855e9648cda4873773d2b4bb93dca
Marc Hoffmann <hoffmann@dti.ch>
2021-07-07 15:56:39 +0200

    Einbau Core-Code basierend auf konstanten Synonym-Onjekten

--- search-parts/src/services/searchService/SharePointSearchService.ts
+++ search-parts/src/services/searchService/SharePointSearchService.ts
@@ -19,6 +19,33 @@ import { Constants } from '../../common/Constants';
 const SearchService_ServiceKey = 'pnpSearchResults:SharePointSearchService';
 const AvailableQueryLanguages_StorageKey = 'pnpSearchResults_AvailableQueryLanguages';
 
+// *********** DTI DEVELOP *************
+// TODO: Interfaces in externe Dateien auslagern und über Import wieder hinzufügen
+// Info: In der SP Liste gibt es 3 Spalten: KeyWord, Synonyms und Mutual;
+// Im Obekt werden aber Keyword und Synonyme in einerSpalte zusammengefast
+interface synonymTableEntry {
+    synonyms: string;
+    mutual: boolean;
+}
+
+interface queryTermsToResolvedSynonymsEntry {
+    queryTerm: string;
+    foundSynonyms: string;
+}
+
+// TODO: Aus SharePoint auslesen und abfüllen... --> Funktion und als private Variable in Klasse?
+// Info: In der SP Liste gibt es 3 Spalten: KeyWord, Synonyms und Mutual;
+// Im Obekt werden aber Keyword und Synonyme in einerSpalte zusammengefast
+let synonymsList = [
+    <synonymTableEntry>{ synonyms: "DTI;DTI Schweiz AG;Document Text Information;Fritz", mutual: true },
+    <synonymTableEntry>{ synonyms: "ACS;Automobilclub Schweiz", mutual: true },
+    <synonymTableEntry>{ synonyms: "Auto;Automobil;Autoclub Deutschland", mutual: true },
+    <synonymTableEntry>{ synonyms: "Bank;Sitzbank;Kantonale Bank", mutual: true},
+    <synonymTableEntry>{ synonyms: "DTI;Marc Hoffmann", mutual: true },
+    <synonymTableEntry>{ synonyms: "Neuigkeiten;News deutsh;News français;News italiano", mutual: true }
+];
+// *********** DTI DEVELOP *************
+
 export class SharePointSearchService implements ISharePointSearchService {
 
     public static ServiceKey: ServiceKey<ISharePointSearchService> = ServiceKey.create(SearchService_ServiceKey, SharePointSearchService);
@@ -70,7 +97,8 @@ export class SharePointSearchService implements ISharePointSearchService {
      */
     public async search(searchQuery: ISharePointSearchQuery): Promise<ISharePointSearchResults> {
 
-        // 20210701/mh: enrich the query test with synonyms...
+        // enrich the query test with synonyms (if enabled)...
+        // TODO: evaluate webparts 'synonyms' enabled config flag --> maybe do here or in enrichFunction
         searchQuery.Querytext = this.enrichQueryWithSynonyms(searchQuery.Querytext);
 
         let results: ISharePointSearchResults = {
@@ -440,18 +468,158 @@ export class SharePointSearchService implements ISharePointSearchService {
         return { results: Array.isArray(prop) ? prop : [prop] };
     }
 
-    //TODO: fertigstellen/ausarbeiten der Funktion
     private enrichQueryWithSynonyms(queryText:string): string {
-        // fetching the original query to enrich it with synonyms...
-        let queryToModify: string = queryText;
+        // storing the original query to use for query expansion bulding...
+        // wee are keeping the case of the queryText to keep AND, NOT,... in their case (operators have to be uppercase to be handled as operators!)
+        let originalQuery: string = queryText;
 
+        // initialize/set default return value to the original query (to be used if no synonyms found)
+        let modifiedQuery: string = queryText;
+
+        // TODO: evaluate webparts 'synonyms' enabled config flag --> maybe already outsite where this function is called
         // only modify the query if it is not undefined, otherwise we would have the string 'undefined' in the query
-        if (queryToModify != undefined) {
-            //HACK : only for development
-            queryToModify = queryToModify + " OR Communication";
+        if (modifiedQuery != undefined) {
+
+            // evaluating the raw query by stripping query operators and converting to lowercase...
+            let rawQuery = originalQuery.replace(/((^|\s)-[\w-]+[\s$])|(-"\w+.*?")|(-?\w+[:=<>]+\w+)|(-?\w+[:=<>]+".*?")|((\w+)?\(.*?\))|(AND)|(OR)|(NOT)/g, " ").replace(/[ ]{2,}/, " ").toLocaleLowerCase();
+            let queryTerms = rawQuery.split(" ");
+
+            //TODO: optimization/generic solution which is not limited to 5 query terms
+            // now prepare all term/word combinations from the raw query to get possible standing terms (like 'United States of America)
+            // (each loop gets forward and backward combinations from the term/word)
+            var termCombinations = [];
+            for (var _i = 0; _i < queryTerms.length; _i++) {
+
+                if (_i >= 0) {
+                    termCombinations.push(<queryTermsToResolvedSynonymsEntry>{ queryTerm: queryTerms[_i] });
+                }
+                if (_i > 0) {
+                    termCombinations.push(<queryTermsToResolvedSynonymsEntry>{ queryTerm: queryTerms[_i - 1] + " " + queryTerms[_i] });
+                }
+                if (_i > 1) {
+                    termCombinations.push(<queryTermsToResolvedSynonymsEntry>{ queryTerm: queryTerms[_i - 2] + " " + queryTerms[_i - 1] + " " + queryTerms[_i] });
+                }
+                if (_i > 2) {
+                    termCombinations.push(<queryTermsToResolvedSynonymsEntry>{ queryTerm: queryTerms[_i - 3] + " " + queryTerms[_i - 2] + " " + queryTerms[_i - 1] + " " + queryTerms[_i] });
+                }
+                if (_i > 3) {
+                    termCombinations.push(<queryTermsToResolvedSynonymsEntry>{ queryTerm: queryTerms[_i - 4] + " " + queryTerms[_i - 3] + " " + queryTerms[_i - 2] + " " + queryTerms[_i - 1] + " " + queryTerms[_i] });
+                }
+                if (_i > 4) {
+                    termCombinations.push(<queryTermsToResolvedSynonymsEntry>{ queryTerm: queryTerms[_i - 5] + " " + queryTerms[_i - 4] + " " + queryTerms[_i - 3] + " " + queryTerms[_i - 2] + " " + queryTerms[_i - 1] + " " + queryTerms[_i] });
+                }
+            }
+
+            // 20210707/mh: sorting is not required amymore, because we check all combinations for synonyms
+            /*
+            termCombinationToAdd.sort((a, b) => {
+                if (a.queryTerm.length < b.queryTerm.length) {
+                    return 1
+                } else if (a.queryTerm.length > b.queryTerm.length) {
+                    return -1
+                }
+                return 0
+            })
+            */
+
+            // now looping through each term/word combination and check if there has been synonyms defined for this combination
+            // (per default synonyms are handled mutual if not specified otherwise)
+            // the synonyms counter us used for later decison if we have fond synonyms over the whole query
+            let termsWithSynonymsCounter = 0;
+            for (let combination of termCombinations) {
+                //TODO: Develop and fill the correct synonyms list object which is prepared from a sharepoint list...
+
+                // do the lookup in the synonym table...
+                let foundSynonymListEntries = synonymsList.filter(entry => entry.synonyms.split(';').filter(synonym => synonym.toLowerCase() == combination.queryTerm).length > 0)
+                if (foundSynonymListEntries.length > 0) {
+                    let synonymsForTerm = ""
+                    for (let listEntry of foundSynonymListEntries) {
+                        let listEntrySynonyms = listEntry.synonyms.toLocaleLowerCase();
+                        if (listEntry.mutual == false) {
+                            // if the synonym list entry is not mutual, only synonyms are taken in conideration, where the 1st synonym ('keyword') matches the term 
+                            if (listEntrySynonyms.indexOf(combination.queryTerm) == 0) {
+                                synonymsForTerm = synonymsForTerm + ";" + listEntrySynonyms.replace(combination.queryTerm, "").replace(";;", ";").replace(/^;+|;+$/g, '');
+                            }
+                        } else {
+                            synonymsForTerm = synonymsForTerm + ";" + listEntrySynonyms.replace(combination.queryTerm, "").replace(";;", ";").replace(/^;+|;+$/g, '');
+                        }
+                    }
+                    // trimming leadidnd trailing semmicolons of the combined string
+                    synonymsForTerm = synonymsForTerm.replace(/^;+|;+$/g, '');
+
+                    // and adding the prepared synonyms to the desired queryTermsToResolvedSynonymsEntry and increasing the counter
+                    combination.foundSynonyms = synonymsForTerm;
+                    termsWithSynonymsCounter++;
+                }
+            }
+
+            // preparing the returned query string:
+            if (termsWithSynonymsCounter > 0) {
+                // only modify the query if we have found any synonyms
+
+                // first we use the original query with all the operators... 
+                modifiedQuery = "(" + originalQuery + ")";
+
+                // ... and adding OR variations of the original query replaced with the synonyms for each term combination...
+                for (let combination of termCombinations) {
+                    
+                    // only process combination entries where we have found synonyms 
+                    if (combination.foundSynonyms) {
+
+                        // split the found synonyms so that we can loop over them and prepare the OR variation with all synonyms
+                        let splittedSynonyms = combination.foundSynonyms.split(';');
+
+                        let orVariation = "";
+                        for (let synonym of splittedSynonyms) {
+                            // multi value synonyms are put in quotation marks
+                            orVariation = orVariation + " OR " + this.formatSynonym(synonym);
+                        }
+                        // removing the first OR from the variation
+                        orVariation = orVariation.replace(/^\sOR\s+|\sOR\s+$/g, '');
+
+                        // if we have more than one element put it in brackets...
+                        if (splittedSynonyms.length > 1) {
+                            orVariation = "(" + orVariation + ")";
+                        }
+
+                        // finally replacing the term combination in the original query
+                        // case insensitive to keep query operators as OR, AND, NOT,... in their case (operators have to be uppercase to be handled as operators!)
+                        var regex = new RegExp(combination.queryTerm, "gi")
+                        let replacedOriginalQuery = originalQuery.replace(regex, orVariation);
+
+                        // putting the original query with replaced synonyms as OR variation to the query string 
+                        modifiedQuery = modifiedQuery + " OR (" + replacedOriginalQuery + ")";
+                    }
+                }
+            } else {
+                // No synonyms found over the whole query --> we keep the initial copy of the original query...
+            }
+        } else {
+            // no query to modify...
         }
 
+        // TODO: maybe remove/comment out in a later stage        
+        // writing some output for debugging purposes
+        console.log("original query: '" + originalQuery + "'");
+        console.log("modified query: '" + modifiedQuery + "'");
+
         // and returning the (un)modified query text...
-        return queryToModify;
+        return modifiedQuery;
+
+        // Develop: expected example:
+        // (Original Query            )    (Variation 1                                       )    (Variation 2                                                                                                                           )
+        // (Marc Hoffmann AND DTI Hans) OR ((Marc Hoffmann AND DTI Hans) OR (dti AND DTI Hans)) OR ((Marc Hoffmann AND DTI Hans) OR (Marc Hoffmann AND ("dti schweiz ag" OR "document text information" OR Fritz OR "marc hoffmann") Hans))
+        // can be reduced to:
+        // (Marc Hoffmann AND DTI Hans) OR (dti AND DTI Hans) OR (Marc Hoffmann AND ("dti schweiz ag" OR "document text information" OR Fritz OR "marc hoffmann") Hans)
+    }
+
+    // Helper function to put multi value terms into quotation marks
+    private formatSynonym(value: string): string {
+        if (!value) return "";
+        value = value.trim().replace(/"/g, '').trim();
+        if (value.indexOf(' ') > -1) {
+            value = '"' + value + '"';
+        }
+        return value;
     }
 }
