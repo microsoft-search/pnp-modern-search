4a25ac33da6ca75d4e567eb28842ceac8bb105c7
Marc Hoffmann <hoffmann@dti.ch>
2021-09-14 11:27:45 +0200

    - Update merged variables and comments from already deployed Filter Fix

--- search-parts/src/dataSources/SharePointSearchDataSource.ts
+++ search-parts/src/dataSources/SharePointSearchDataSource.ts
@@ -1,4 +1,4 @@
-import * as React from 'react';
+﻿import * as React from 'react';
 import { IDataSourceData, BaseDataSource, ITokenService, IDataFilter, ITemplateSlot, IDataFilterResult, IDataFilterResultValue, BuiltinTemplateSlots, FilterComparisonOperator, IDataFilterConfiguration, FilterBehavior, FilterSortType, FilterSortDirection } from "@pnp/modern-search-extensibility";
 import {
     IPropertyPaneGroup,
@@ -767,6 +767,7 @@ export class SharePointSearchDataSource extends BaseDataSource<ISharePointSearch
 
         searchQuery.EnableQueryRules = this.properties.enableQueryRules;
         searchQuery.QueryTemplate = await this._tokenService.resolveTokens(this.properties.queryTemplate);
+
         if (this.properties.resultSourceId) {
             searchQuery.SourceId = this.properties.resultSourceId;
         }
@@ -1051,26 +1052,26 @@ export class SharePointSearchDataSource extends BaseDataSource<ISharePointSearch
                     values.forEach((term) => {
 
                         // 20210811: We strip the language specific part of the term and use the GP0 value as the filter value
-                        // and use also the striped value as the new term/key...
-                        // --> 'GP0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3' instead of 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Category 1' for the name
-                        // --> 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3' instead of 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Category 1' for the name
-                        // Background: if the same term is used on sites with different default language the indexed taxid property value contains the 
+                        // and use also the stripped value as the new term/key...
+                        // --> 'GP0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3' instead of 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Category 1' for the value/filter
+                        // --> 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|' instead of 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Category 1' for the name
+                        // Background: if the same term is used on sites with different default language, the indexed taxid property value contains the 
                         // translation of that site collection and therefore it comes back as different values meaning the same with the same id but causing
                         // separate filters.
-                        // (Example: 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Food' 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Nahrung')
+                        // (Example: 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Food' and 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Nahrung')
                         let matches = /L0\|#(.+)\|/.exec(value.name);
                         let termId = Guid.isValid(matches[1]) ? matches[1] : matches[1].substr(1);
-                        let stripedTermFilter = "GP0|#" + termId.toString();
-                        let stripedTerm = term.substring(0,term.lastIndexOf('|')+1); // (we keep the last pipe for compatibility with further code/regexp)
+                        let strippedTermFilter = "GP0|#" + termId.toString();
+                        let strippedTerm = term.substring(0,term.lastIndexOf('|')+1); // (we keep the last pipe for compatibility with further code/regexp)
 
-                        const fqlFilterValue = `"ǂǂ${this._bytesToHex(this._stringToUTF8Bytes(stripedTermFilter))}"`;
-                        const existingFilterIdx = updatedValues.map(updatedValue => updatedValue.name).indexOf(stripedTerm);
+                        const fqlFilterValue = `"ǂǂ${this._bytesToHex(this._stringToUTF8Bytes(strippedTermFilter))}"`;
+                        const existingFilterIdx = updatedValues.map(updatedValue => updatedValue.name).indexOf(strippedTerm);
 
                         if (existingFilterIdx === -1) {
                             // Create a dedicated filter value entry
                             updatedValues.push({
                                 count: value.count,
-                                name: stripedTerm, // New stripped term, ex: 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3' instead of 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Category 1'
+                                name: strippedTerm, // New stripped term, ex: 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3' instead of 'L0|#a2cf1afb-44b6-4cf4-bf37-642bb2e9bff3|Category 1'
                                 value: fqlFilterValue
                             } as IDataFilterResultValue);
 
@@ -1180,7 +1181,7 @@ export class SharePointSearchDataSource extends BaseDataSource<ISharePointSearch
                     const existingFilters = localizedTerms.filter((e) => { return e.uniqueIdentifier === value.value; });
 
                     if (existingFilters.length > 0) {
-                        // 20210811: because we heve modified the filter value with the GP0 item the mapping also returns items which are not L0
+                        // 20210811: because we have modified the filter value to use the GP0 also for the L0 items, the mapping also returns items which are not L0
                         // therefore we have to check the name for L0 again and only replace those...
                         const isTerm = /L0\|#(.+)\|/.test(value.name);
                         if (isTerm) {
