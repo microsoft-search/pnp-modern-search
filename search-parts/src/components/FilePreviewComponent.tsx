fb470f277b65a192dc54722bd7ea9137b58a9c35
Mikael Svenson <misvenso@microsoft.com>
2021-09-19 14:37:12 +0200

    Only use interactive preview on same domain Fallback to image URL on different domain

--- search-parts/src/components/FilePreviewComponent.tsx
+++ search-parts/src/components/FilePreviewComponent.tsx
@@ -44,50 +44,64 @@ export class FilePreview extends React.Component<IFilePreviewProps, IFileIconSta
             isCalloutVisible: false
         };
     }
-    
+
     public render() {
 
         let renderPreviewCallout: JSX.Element = null;
 
-        if (this.state.isCalloutVisible && this.props.previewUrl) {
+        let previewUrl = this.props.previewUrl;
 
+        // Fallback to thumbnail in iframe if different domain as auth won't work cross domains
+        if(previewUrl && this.props.previewImageUrl && !this.isCurrentDomain(previewUrl)) {
+            previewUrl = this.props.previewImageUrl;
+        }
+
+        if (!this.props.previewImageUrl) {
+            return null;
+        }
+
+        if (this.state.isCalloutVisible && previewUrl) {
             renderPreviewCallout = <PreviewContainer
-                elementUrl={UrlHelper.decode(this.props.previewUrl)}
-                previewImageUrl={this.props.previewImageUrl ? UrlHelper.decode(this.props.previewImageUrl) : UrlHelper.decode(this.props.previewUrl)}
+                elementUrl={UrlHelper.decode(previewUrl)}
+                previewImageUrl={this.props.previewImageUrl ? UrlHelper.decode(this.props.previewImageUrl) : UrlHelper.decode(previewUrl)}
                 targetElement={this.elementPreviewRef.current}
                 previewType={PreviewType.Document}
                 showPreview={this.state.isCalloutVisible}
             />;
-        }   
-
-        if(!this.props.previewImageUrl) {
-            return null;
         }
 
-        return  <div>
-                    <div 
-                        ref={this.elementPreviewRef} 
-                        dangerouslySetInnerHTML={ { __html: DOMPurify.default.sanitize(this.props.template) }} 
-                        onClick={() => {
-                            this.setState({
-                                isCalloutVisible: true
-                            });
-                        }}/>
-                    {renderPreviewCallout}
-                </div>;
+        return <div>
+            <div
+                ref={this.elementPreviewRef}
+                dangerouslySetInnerHTML={{ __html: DOMPurify.default.sanitize(this.props.template) }}
+                onClick={() => {
+                    this.setState({
+                        isCalloutVisible: true
+                    });
+                }} />
+            {renderPreviewCallout}
+        </div>;
+    }
+
+    /**
+     * Check if we're on the same domain
+     * @param domain
+     */
+    private isCurrentDomain(url: string) {
+        return url && url.toLocaleLowerCase().indexOf(window.location.hostname.toLocaleLowerCase()) !== -1;
     }
 }
 
 export class FilePreviewWebComponent extends BaseWebComponent {
-   
+
     public constructor() {
-        super(); 
+        super();
     }
- 
+
     public async connectedCallback() {
 
-       let props = this.resolveAttributes();
-       const filePreview = <FilePreview {...props} template={this.innerHTML}/>;
-       ReactDOM.render(filePreview, this);
-    }    
+        let props = this.resolveAttributes();
+        const filePreview = <FilePreview {...props} template={this.innerHTML} />;
+        ReactDOM.render(filePreview, this);
+    }
 }
