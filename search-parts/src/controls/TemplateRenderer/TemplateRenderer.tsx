581b9e4e816926a9e74130ef67a6b5d4aaa4b8f9
Mikael Svenson <misvenso@microsoft.com>
2021-10-13 12:25:11 +0200

    Fix dom purify settings

--- search-parts/src/controls/TemplateRenderer/TemplateRenderer.tsx
+++ search-parts/src/controls/TemplateRenderer/TemplateRenderer.tsx
@@ -17,7 +17,7 @@ export class TemplateRenderer extends React.Component<ITemplateRendererProps, IT
 
     constructor(props: ITemplateRendererProps) {
         super(props);
-    
+
         this.state = {
             processedTemplate: null
         };
@@ -26,21 +26,22 @@ export class TemplateRenderer extends React.Component<ITemplateRendererProps, IT
 
         this._domPurify.setConfig({
             ADD_TAGS: ['style'],
-            ADD_ATTR: ['onerror', 'target', 'loading'],
+            ADD_ATTR: ['target', 'loading'],
+            FORBID_ATTR: ['onclick', 'onerror', 'onfocus'],
             ALLOWED_URI_REGEXP: /^(?:(?:(?:f|ht)tps?|mailto|file|tel|callto|cid|xmpp|xxx|ms-\w+):|[^a-z]|[a-z+.\-]+(?:[^a-z+.\-:]|$))/i,
-            WHOLE_DOCUMENT: true
+            WHOLE_DOCUMENT: false,
         });
 
         this._domPurify.addHook('uponSanitizeElement', DomPurifyHelper.allowCustomComponentsHook);
-        this._domPurify.addHook('uponSanitizeAttribute', DomPurifyHelper.allowCustomAttributesHook);        
+        this._domPurify.addHook('uponSanitizeAttribute', DomPurifyHelper.allowCustomAttributesHook);
     }
 
     public render() {
 
-       
+
         if (this.state.processedTemplate) {
-             // We need to set a key to force an update if Wthe template context changes
-            return  <div className={rootCssClassName} dangerouslySetInnerHTML={{__html: this.state.processedTemplate }}></div>;
+            // We need to set a key to force an update if Wthe template context changes
+            return <div className={rootCssClassName} dangerouslySetInnerHTML={{ __html: this.state.processedTemplate }}></div>;
         } else {
             return null;
         }
@@ -58,7 +59,6 @@ export class TemplateRenderer extends React.Component<ITemplateRendererProps, IT
     }
 
     private async updateTemplate(props: ITemplateRendererProps): Promise<void> {
-
         let templateContent = props.templateContent;
 
         // Process the Handlebars template
@@ -71,9 +71,9 @@ export class TemplateRenderer extends React.Component<ITemplateRendererProps, IT
             const templateAsHtml = new DOMParser().parseFromString(template, "text/html");
 
             // Get <style> tags from Handlebars template content and prefix all CSS rules by the Web Part instance ID to isolate styles
-            const styleElements = templateAsHtml.getElementsByTagName("style"); 
+            const styleElements = templateAsHtml.getElementsByTagName("style");
             let prefixedStyles: string[] = [];
-            let i,j,k = 0;
+            let i, j, k = 0;
 
             if (styleElements.length > 0) {
 
@@ -82,19 +82,19 @@ export class TemplateRenderer extends React.Component<ITemplateRendererProps, IT
 
                 for (i = 0; i < styleElements.length; i++) {
                     const style = styleElements.item(i);
-                    const sheet: any = style.sheet; 
+                    const sheet: any = style.sheet;
                     if ((sheet as CSSStyleSheet).cssRules) {
                         const cssRules = (sheet as CSSStyleSheet).cssRules;
-                        
+
                         for (j = 0; j < cssRules.length; j++) {
                             const cssRule: CSSRule = cssRules.item(j);
-                            
+
                             // CSS Media rule
                             if ((cssRule as CSSMediaRule).media) {
                                 const cssMediaRule = cssRule as CSSMediaRule;
 
                                 let cssPrefixedMediaRules = '';
-                                for (k= 0; k < cssMediaRule.cssRules.length; k++) {
+                                for (k = 0; k < cssMediaRule.cssRules.length; k++) {
                                     const cssRuleMedia = cssMediaRule.cssRules.item(k);
                                     cssPrefixedMediaRules += `#${elementPrefixId} ${cssRuleMedia.cssText}`;
                                 }
@@ -108,13 +108,13 @@ export class TemplateRenderer extends React.Component<ITemplateRendererProps, IT
                     }
 
                     // Remove the element from DOM
-                    style.remove(); 
+                    style.remove();
                 }
             }
 
             template = `<style>${prefixedStyles.join(' ')}</style><div id="${TEMPLATE_ID_PREFIX}${this.props.instanceId}">${templateAsHtml.body.innerHTML}</div>`;
         }
-            
+
         this.setState({
             processedTemplate: template
         });
