6cf6c21766f538332c0b21d1e13ca2dbdf52a056
Magnus Ellinge <magnus.ellinge@consid.se>
2021-11-17 23:49:08 +0100

    Update language files

--- search-parts/src/loc/en-us.js
+++ search-parts/src/loc/en-us.js
@@ -162,7 +162,7 @@ define([], function() {
             TextFieldApplyButtonText: "Apply",
             ExportButtonText: "Export (csv)",
             ExportInfoText: "Information",
-            ExportDialogHelpText: "Select if you want to export the current page/view or all the data (with a maximum of {maxhits} rows). Notice that there can be a difference in shown columns, value formatting and applied sorting in the outputted file compared to current view.",
+            ExportDialogHelpText: "Select if you want to export the current page/view or all the data (with a maximum of {maxhits} rows). Notice that there can be a difference in shown columns, value formatting and applied sorting in the outputted file compared to current view. There is also no guarantee that the exported content has included all potential hits.",
             ExportBrowserNotSupportedText: "Browser is not supported. Please try again with another browser.",
             ExportCurrentPageLabel: "Current page/view",
             ExportAllLabel: "All (max {maxhits} rows)",
