4a44f4c2e521b2f71c2c452f421891d6e258d723
Mikael Svenson <miksvenson@gmail.com>
2021-12-16 15:11:14 +0100

    #1649 #1650 re-add accessibility

--- search-parts/src/components/CollapsibleContentComponent.tsx
+++ search-parts/src/components/CollapsibleContentComponent.tsx
@@ -62,11 +62,12 @@ export class CollapsibleContentComponent extends React.Component<ICollapsibleCon
 
         this._onRenderCell = this._onRenderCell.bind(this);
         this._onRenderHeader = this._onRenderHeader.bind(this);
+        this._onTogglePanel = this._onTogglePanel.bind(this);
 
         this._domPurify = DOMPurify.default;
     }
 
-    
+
     public render() {
 
         const groups: IGroup[] = [
@@ -75,77 +76,85 @@ export class CollapsibleContentComponent extends React.Component<ICollapsibleCon
                 name: this.props.groupName,
                 count: 1, // The count should be at least 1 to show the header
                 startIndex: 0,
-                isShowingAll:  true, 
-                hasMoreData: false,               
+                isShowingAll: true,
+                hasMoreData: false,
                 isCollapsed: this.state.isCollapsed,
             }
         ];
 
         const groupedList = <GroupedList
-                                items={[
-                                    <div dangerouslySetInnerHTML={{ __html: this._domPurify.sanitize(this.props.contentTemplate)}}></div>
-                                ]}
-                                styles={{
-                                    root: {
-                                        selectors: {
-                                            '.ms-List-cell': {
-                                                minHeight: 0
-                                            }
-                                        }
-                                    }
-                                }}
-                                onRenderCell={this._onRenderCell}
-                                className={styles.collapsible__filterPanel__body__group}     
-                                onShouldVirtualize={() => false}
-                                listProps={ { onShouldVirtualize: () => false } }           
-                                groupProps={
-                                    {  
-                                        onRenderHeader: this._onRenderHeader,
-                                        onRenderFooter: ((props) => {
+            items={[
+                <div dangerouslySetInnerHTML={{ __html: this._domPurify.sanitize(this.props.contentTemplate) }}></div>
+            ]}
+            styles={{
+                root: {
+                    selectors: {
+                        '.ms-List-cell': {
+                            minHeight: 0
+                        }
+                    }
+                }
+            }}
+            onRenderCell={this._onRenderCell}
+            className={styles.collapsible__filterPanel__body__group}
+            onShouldVirtualize={() => false}
+            listProps={{ onShouldVirtualize: () => false }}
+            groupProps={
+                {
+                    onRenderHeader: this._onRenderHeader,
+                    onRenderFooter: ((props) => {
 
-                                            if (!props.group.isCollapsed) {
-                                                return <div dangerouslySetInnerHTML={{ __html: this._domPurify.sanitize(this.props.footerTemplate)}}></div>;
-                                            } else {
-                                                return null;
-                                            }                                        
-                                        }).bind(this),
-                                    }
-                                }
-                                groups={groups} 
-                            />;
+                        if (!props.group.isCollapsed) {
+                            return <div dangerouslySetInnerHTML={{ __html: this._domPurify.sanitize(this.props.footerTemplate) }}></div>;
+                        } else {
+                            return null;
+                        }
+                    }).bind(this),
+                }
+            }
+            groups={groups}
+        />;
 
         return <div ref={this.componentRef} data-name={this.props.groupName} data-is-scrollable={true}>{groupedList}</div>;
     }
-      
+
+    private _onTogglePanel(props: IGroupDividerProps) {
+        this.setState({
+            isCollapsed: !props.group.isCollapsed
+        });
+        props.onToggleCollapse(props.group);
+    }
+
     private _onRenderHeader(props: IGroupDividerProps): JSX.Element {
 
         return (
-            <div style={{position: 'relative'}}>
-                <div    
+            <div style={{ position: 'relative' }}>
+                <div
                     className={styles.collapsible__filterPanel__body__group__header}
                     onClick={() => {
-
-                        this.setState({
-                            isCollapsed: !props.group.isCollapsed
-                        });
-
-                        props.onToggleCollapse(props.group);
-                    }}>
+                        this._onTogglePanel(props);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.charCode === 13) {
+                            this._onTogglePanel(props);
+                        }
+                    }}
+                >
                     <Text variant={'large'}>{props.group.name}</Text>
                     <div className={styles.collapsible__filterPanel__body__headerIcon}>
-                    {props.group.isCollapsed ?
-                        <Icon iconName='ChevronDown' />
-                        :
-                        <Icon iconName='ChevronUp' />
-                    }
+                        {props.group.isCollapsed ?
+                            <Icon iconName='ChevronDown' />
+                            :
+                            <Icon iconName='ChevronUp' />
+                        }
                     </div>
                 </div>
-                {!props.group.isCollapsed ? 
-                    <div dangerouslySetInnerHTML={{ __html: this._domPurify.sanitize(this.props.headerTemplate)}}></div>
-                : 
-                    null 
+                {!props.group.isCollapsed ?
+                    <div dangerouslySetInnerHTML={{ __html: this._domPurify.sanitize(this.props.headerTemplate) }}></div>
+                    :
+                    null
                 }
-            </div>   
+            </div>
         );
     }
 
@@ -159,18 +168,18 @@ export class CollapsibleContentComponent extends React.Component<ICollapsibleCon
 }
 
 export class CollapsibleContentWebComponent extends BaseWebComponent {
-   
+
     public constructor() {
-        super(); 
+        super();
     }
- 
+
     public async connectedCallback() {
 
         const domParser = new DOMParser();
         const htmlContent: Document = domParser.parseFromString(this.innerHTML, 'text/html');
 
         // Get the templates
-        const headerTemplateContent = htmlContent.getElementById('collapsible-header');        
+        const headerTemplateContent = htmlContent.getElementById('collapsible-header');
         const contentTemplateContent = htmlContent.getElementById('collapsible-content');
         const footerTemplateContent = htmlContent.getElementById('collapsible-footer');
 
@@ -181,7 +190,7 @@ export class CollapsibleContentWebComponent extends BaseWebComponent {
         if (contentTemplateContent) {
             contentTemplate = contentTemplateContent.innerHTML;
         }
-        
+
         if (footerTemplateContent) {
             footerTemplate = footerTemplateContent.innerHTML;
         }
@@ -191,13 +200,13 @@ export class CollapsibleContentWebComponent extends BaseWebComponent {
         }
 
         let props = this.resolveAttributes();
-        const collapsibleContent =  <CollapsibleContentComponent 
-                                        {...props} 
-                                        headerTemplate={headerTemplate}
-                                        contentTemplate={contentTemplate} 
-                                        footerTemplate={footerTemplate}
-                                    />;
+        const collapsibleContent = <CollapsibleContentComponent
+            {...props}
+            headerTemplate={headerTemplate}
+            contentTemplate={contentTemplate}
+            footerTemplate={footerTemplate}
+        />;
 
         ReactDOM.render(collapsibleContent, this);
-    }    
+    }
 }
