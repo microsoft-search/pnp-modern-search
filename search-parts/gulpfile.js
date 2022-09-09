bb9ceb74a2add7892e43ae384f63640b910a5f4b
Franck Cornu <franck.cornu@ubisoft.com>
2022-09-09 10:25:21 -0400

    Added GitHub actions workflow for WebParts release

--- search-parts/gulpfile.js
+++ search-parts/gulpfile.js
@@ -119,3 +119,62 @@ build.rig.getTasks = function() {
 };
 
 build.initialize(require('gulp'));
+
+const findFilesByExt = (base, ext, files, result) => {
+    files = files || fs.readdirSync(base) 
+    result = result || [] 
+
+    files.forEach( 
+        function (file) {
+            var newbase = path.join(base,file)
+            if (fs.statSync(newbase).isDirectory()) {
+                result = findFilesByExt(newbase,ext,fs.readdirSync(newbase),result)
+            } else {
+                if ( file.substr(-1*(ext.length+1)) == '.' + ext ) {
+                    result.push(newbase)
+                } 
+            }
+        }
+    );
+    return result
+}
+
+gulp.task('update-version', async () => {
+
+  // List all manifest files
+  const manifestFiles = findFilesByExt('./src','manifest.json');
+
+  const semver = require('semver')
+  const versionArgIdx = process.argv.indexOf('--value');
+  const newVersionNumber = semver.valid(process.argv[versionArgIdx+1]);
+
+  if (versionArgIdx !== -1 && newVersionNumber) {
+      
+      // Update version in the package-solution
+      const pkgSolutionFilePath = './config/package-solution.json';
+      
+      readJson(pkgSolutionFilePath, (err, pkgSolution) => {
+        log.info('Old package-solution.json version:\t' + pkgSolution.solution.version);
+        const pkgVersion = `${semver.major(newVersionNumber)}.${semver.minor(newVersionNumber)}.${semver.patch(newVersionNumber)}.0`;
+        pkgSolution.solution.version = pkgVersion
+        log.info('New package-solution.json version:\t' + pkgVersion);
+        fs.writeFile(pkgSolutionFilePath, JSON.stringify(pkgSolution, null, 4), (error) => {});  
+      });
+
+      // Updated version in Web Part manifests
+      manifestFiles.forEach((manifestFile) => {
+        readJson(`./${manifestFile}`, (err, manifest) => {
+
+          log.info(`Updating manifest file: "./${manifestFile}"`);
+
+          log.info('Old manifestFile version:\t' + manifest.version);
+          const wpVersion = `${semver.major(newVersionNumber)}.${semver.minor(newVersionNumber)}.${semver.patch(newVersionNumber)}`;
+          manifest.version = wpVersion;
+          log.info('New manifestFile version:\t' + wpVersion);
+          fs.writeFile(manifestFile, JSON.stringify(manifest, null, 4), (error) => {});  
+        });
+      });
+  } else {
+      log.error(`The provided version ${process.argv[versionArgIdx+1]} is not a valid SemVer version`);
+  }
+});
