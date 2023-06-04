d47dd61ad8993b0161942d7449b82ddf36ae6a95
Mikael Svenson <misvenso@microsoft.com>
2023-06-04 11:54:26 +0200

    Ensure locale is re-set on translated pages

--- search-parts/src/helpers/DateHelper.ts
+++ search-parts/src/helpers/DateHelper.ts
@@ -11,6 +11,8 @@ export class DateHelper {
 
     private pageContext: PageContext;
 
+    private lastCulture: string;
+
     public constructor(serviceScope: ServiceScope) {
         serviceScope.whenFinished(() => {
             this.pageContext = serviceScope.consume<PageContext>(PageContext.serviceKey);
@@ -19,19 +21,12 @@ export class DateHelper {
 
     public async moment(): Promise<any> {
 
-        if (this.momentLibrary) {
-            return Promise.resolve(this.momentLibrary);
-        } else {
-            let moment: any = await import(
-                /* webpackMode: 'lazy', webpackChunkName: 'pnp-modern-search-moment-base' */
-                'moment'
-            );
+        let culture = LocalizationHelper.getTranslatedCultureFromUrl();
+        if (!culture) {
+            culture = this.pageContext.cultureInfo.currentUICultureName.toLowerCase();
+        }
 
-            let culture = LocalizationHelper.getTranslatedCultureFromUrl();
-            if (!culture) {
-                culture = this.pageContext.cultureInfo.currentUICultureName.toLowerCase();
-            }
-
+        if (!this.lastCulture || this.lastCulture !== culture) {
             // if culture starts with or is any of this, two letter locale must be used in momentjs (culture es-es must load es.js file)
             let momentTwoLetterLanguageName = [
                 "af", "az", "be", "bg", "bm", "bo", "br", "bs", "ca", "cs", "cv",
@@ -59,6 +54,18 @@ export class DateHelper {
                     `moment/locale/${culture}.js`
                 );
             }
+            this.lastCulture = culture;
+        }
+
+
+        if (this.momentLibrary) {
+            this.momentLibrary.locale(culture);
+            return Promise.resolve(this.momentLibrary);
+        } else {
+            let moment: any = await import(
+                /* webpackMode: 'lazy', webpackChunkName: 'pnp-modern-search-moment-base' */
+                'moment'
+            );
 
             // Needs to use 'default' due to webpack 4.
             this.momentLibrary = moment.default;
