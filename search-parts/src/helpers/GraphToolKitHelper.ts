4fec05cdca08775f0a6ed00685d0cecc2d21a257
Mikael Svenson <misvenso@microsoft.com>
2023-09-28 13:42:36 +0200

    Update mgt component

--- search-parts/src/helpers/GraphToolKitHelper.ts
+++ search-parts/src/helpers/GraphToolKitHelper.ts
@@ -5,19 +5,22 @@ export const loadMsGraphToolkit = async (context: WebPartContext) => {
     const component = window.customElements.get("mgt-mock-provider");
     if (!component) {
         // Load Microsoft Graph Toolkit dynamically
-        const { Providers } = await import(
-            /* webpackChunkName: 'microsoft-graph-toolkit' */
-            '@microsoft/mgt-react/dist/es6'
-        );
-
-        if (!Providers.globalProvider) {
-            const { SharePointProvider } = await import(
+        try {
+            const { Providers } = await import(
                 /* webpackChunkName: 'microsoft-graph-toolkit' */
-                '@microsoft/mgt-sharepoint-provider/dist/es6'
+                '@microsoft/mgt-spfx'
             );
-
-            Providers.globalProvider = new SharePointProvider(context);
+    
+            if (!Providers.globalProvider) {
+                const { SharePointProvider } = await import(
+                    /* webpackChunkName: 'microsoft-graph-toolkit' */
+                    '@microsoft/mgt-spfx'
+                );
+    
+                Providers.globalProvider = new SharePointProvider(context);
+            }
+        } catch (error) {
+            console.warn(error);
         }
-
     }
 }
