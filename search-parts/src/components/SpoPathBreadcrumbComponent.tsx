d3db91b69fb694117fb823eacce3fbb3b679d3f4
lauribohm <lauri.bohm@sulava.com>
2024-01-17 11:12:49 +0200

    clean code

--- search-parts/src/components/SpoPathBreadcrumbComponent.tsx
+++ search-parts/src/components/SpoPathBreadcrumbComponent.tsx
@@ -88,15 +88,15 @@ export class SpoPathBreadcrumb extends React.Component<IBreadcrumbProps, IBreadc
             },
         };
 
-        const items = this.validateFilePath(originalPath, spWebUrl) ? this.getBreadcrumbItems(spWebUrl, originalPath, includeSiteName, includeEntityName, breadcrumbItemsAsLinks) : undefined;
+        const breadcrumbItems = this.validateFilePath(originalPath, spWebUrl) ? this.getBreadcrumbItems(spWebUrl, originalPath, includeSiteName, includeEntityName, breadcrumbItemsAsLinks) : undefined;
 
         return (
             <>
-            {items !== undefined &&
+            {breadcrumbItems !== undefined &&
                 <Breadcrumb
-                    items={items}
+                    items={breadcrumbItems}
                     maxDisplayedItems={maxDisplayedItems}
-                    overflowIndex={items.length <= overflowIndex ? 0 : overflowIndex}
+                    overflowIndex={breadcrumbItems.length <= overflowIndex ? 0 : overflowIndex}
                     styles={breadcrumbStyles}
                     ariaLabel="Breadcrumb path"
                     overflowAriaLabel="More links"
@@ -123,7 +123,8 @@ export class SpoPathBreadcrumb extends React.Component<IBreadcrumbProps, IBreadc
 
         // If includeSiteName is true, then only remove the base path from the original path so first items of path are "sitename", "subsite"
         // If includeSiteName is false, then remove the whole spWebUrl from the original path so first item of path is "Shared Documents"
-        const parts = includeSiteName ? originalPath.replace(basePath, '').split('/').filter(part => part) : originalPath.replace(spWebUrl, '').split('/').filter(part => part);
+        const replacePath = includeSiteName ? basePath : spWebUrl;
+        const parts = originalPath.replace(replacePath, '').split('/').filter(part => part);
 
         // If includeEntityName is false, then remove the last part of the path e.g. Document.doxc. Last part is the entity for which the breadcrumb is generated.
         if (!includeEntityName) parts.pop();
@@ -135,9 +136,13 @@ export class SpoPathBreadcrumb extends React.Component<IBreadcrumbProps, IBreadc
             };
 
             // If breadcrumbItemsAsLinks is true, then add the href property to the breadcrumb item
-            // If includeSiteName is true, then the href is the base path + the current path part because parts contain the site name and possible subsite(s)
-            // If includeSiteName is false, then the href is the spWebUrl + the current path part because parts do not contain the site name and possible subsite(s)
-            if (breadcrumbItemsAsLinks) item.href = includeSiteName ? `${basePath}/${parts.slice(0, index + 1).join('/')}` : `${spWebUrl}/${parts.slice(0, index + 1).join('/')}`;
+            if (breadcrumbItemsAsLinks) {
+                const relativePath = parts.slice(0, index + 1).join('/');
+
+                // If includeSiteName is true, then the href is the base path + the current path part because parts contain the site name and possible subsite(s)
+                // If includeSiteName is false, then the href is the spWebUrl + the current path part because parts do not contain the site name and possible subsite(s)
+                item.href = includeSiteName ? `${basePath}/${relativePath}` : `${spWebUrl}/${relativePath}`;
+            }
             
             return item;
         }); 
